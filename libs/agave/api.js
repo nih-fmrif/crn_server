65da132fe166803040963055e4cf2b72d65a85b0
Denes <dsdenes@gmail.com>
2017-07-06 00:00:06 +0200

    refactor: Make agave api use the new request wrapper implementation
    Signed-off-by: Denes <dsdenes@gmail.com>

--- libs/agave/api.js
+++ libs/agave/api.js
@@ -1,6 +1,6 @@
 /*eslint no-console: ["error", { allow: ["log"] }] */
 
-import request from '../request';
+import request from '../request-async';
 import config  from '../../config';
 import client  from './client';
 
